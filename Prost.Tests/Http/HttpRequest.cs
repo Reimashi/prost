cae4a186626f99b1a779fcefdd823497c9f6392d
Aitor González Fernández <reimashi@gmail.com>
2016-09-21 12:43:16 +0200

    Project restructuring

---
+++ Prost.Tests/Http/HttpRequest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Prost.Tests
+{
+	[TestFixture ()]
+	public class HttpRequest
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
---
+++ Prost/Http/HttpConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prost.Http
+{
+    public static class HttpConstants
+    {
+        /*
+        OCTET          = <any 8-bit sequence of data>
+       CHAR           = <any US-ASCII character (octets 0 - 127)>
+       UPALPHA        = <any US-ASCII uppercase letter "A".."Z">
+       LOALPHA        = <any US-ASCII lowercase letter "a".."z">
+       ALPHA          = UPALPHA | LOALPHA
+       DIGIT          = <any US-ASCII digit "0".."9">
+       CTL            = <any US-ASCII control character
+                        (octets 0 - 31) and DEL (127)>
+       CR             = <US-ASCII CR, carriage return (13)>
+       LF             = <US-ASCII LF, linefeed (10)>
+       SP             = <US-ASCII SP, space (32)>
+       HT             = <US-ASCII HT, horizontal-tab (9)>
+       <">            = <US-ASCII double-quote mark (34)>
+        */
+    }
+}
